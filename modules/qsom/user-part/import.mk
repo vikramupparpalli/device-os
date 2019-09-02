6fe4827c4974d111c7f25893a3945c3cf7b76f73
Eugene <eugene@particle.io>
2019-09-02 14:57:54 +0800

    [BG96] Rename PLATFORM_NAME from bg96 to qsom to support all Quectel plaforms

--- modules/qsom/user-part/import.mk
+++ modules/qsom/user-part/import.mk
@@ -1,5 +1,5 @@
 include ../../shared/system_module_version.mk
-USER_PART_MODULE_PATH ?= $(PROJECT_ROOT)/modules/bg96/user-part
+USER_PART_MODULE_PATH ?= $(PROJECT_ROOT)/modules/qsom/user-part
 include $(call rwildcard,$(USER_PART_MODULE_PATH)/,include.mk)
 
 
